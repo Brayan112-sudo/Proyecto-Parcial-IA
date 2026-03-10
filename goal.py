89458b5c56449fb922848ff04b61c53fd8531c71
Brayan112-sudo <brayansolano099@gmail.com>
2026-03-09 21:10:25 -0400

    Este es el Link del video

--- goal.py
+++
@@ -1,7 +0,0 @@
-import pygame;
-
-class Goal(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.image = pygame.image.load("assets/Goal.png").convert_alpha()
-        self.rect = self.image.get_rect(topleft=(x, y))
